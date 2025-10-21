67a2c766aba366973cd3fb2acf78202d907e9329
煌斗 波照間 <c24021@std.it-college.ac.jp>
2025-10-21 12:09:15 +0900

    マス管理仮実装v1

---
+++ Assets/HK/GridManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    int width = 8;
+    int height = 8;
+
+    [SerializeField, Header("マスプレハブ")]
+    GameObject cellPrefab;
+
+    CellScript[,] cellSC;
+
+    void Start()
+    {
+        cellSC = new CellScript[width, height];//配列初期化
+
+        //マップ生成＆各マススクリプト取得
+        for(int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                var cellObj = Instantiate(cellPrefab, new Vector3(x, 0, y), Quaternion.identity);
+                cellSC[y, x] = cellObj.GetComponent<CellScript>();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 指定したマスに移動予約をする
+    /// </summary>
+    /// <param name="y">y座標</param>
+    /// <param name="x">x座標</param>
+    /// <param name="unitSC">呼びだす側のユニットスクリプト</param>
+    public void ReserveCell(int y, int x,CharaScript unitSC)
+    {
+        cellSC[y, x].ReserveState(unitSC);
+    }
+
+    /// <summary>
+    /// 指定したマスの状態を変更する
+    /// </summary>
+    /// <param name="y">y座標(縦の列)</param>
+    /// <param name="x">x座標(横の列)</param>
+    /// <param name="state">状態(empty=空,player=プレイヤー,enemy=敵,damageTile=ダメージ床)</param>
+    /// <param name="unitSC">呼びだす側のユニットスクリプト</param>
+    public void ChangeCellState(int y, int x ,CellScript.CellState state,CharaScript unitSC)
+    {
+        cellSC[y, x].MoveCheck(state,unitSC);
+    }
+
+    /// <summary>
+    /// 指定したマスの状態を確認する
+    /// </summary>
+    /// <param name="y">y座標(縦の列)</param>
+    /// <param name="x">x座標(横の列)</param>
+    /// <returns>現在のマスの状態</returns>
+    public CellScript.CellState CheckCellState(int y, int x)
+    {
+        return cellSC[y, x].CheckState();
+    }
+
+    /// <summary>
+    /// 指定したマスの予約の有無を確認する
+    /// </summary>
+    /// <param name="y">y座標</param>
+    /// <param name="x">x座標</param>
+    /// <returns>予約あり=true, 予約なし=false</returns>
+    public bool CheckCellReserve(int y, int x) => cellSC[y, x].CheckReserve();
+
+    /// <summary>
+    /// すべてのマスの予約をリセット(1行動ごとの終了時に呼ぶ)
+    /// </summary>
+    public void ResetReserveListAll()
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                cellSC[y, x].ResetList();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 指定したマスから離れる
+    /// </summary>
+    /// <param name="y">y座標</param>
+    /// <param name="x">x座標</param>
+    public void LeaveCell(int y, int x)
+    {
+        cellSC[y, x].Leave();
+    }
+}
